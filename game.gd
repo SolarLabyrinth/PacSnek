a86ac20769b0a48f62051fe4e0c2c329a6add82f
SolarLabyrinth <thesolarlabyrinth@gmail.com>
2024-12-15 18:15:49 -0600

    dict

--- game.gd
+++ game.gd
@@ -16,7 +16,7 @@ const GHOST  = Vector2i(7, 0)
 
 const GHOST_COUNT = 4
 
-enum Facing {
+enum Direction {
 	None,
 	Left,
 	Right,
@@ -24,46 +24,67 @@ enum Facing {
 	Down
 }
 
+var dirDictionary = {
+	Direction.None: {
+		"sprite": HEAD_UP
+	},
+	Direction.Up: {
+		"sprite": HEAD_UP,
+		"movement": Vector2i.UP,
+		"action": "ui_up",
+		"long_forbidden_direction": Direction.Down
+	},
+	Direction.Down: {
+		"sprite": HEAD_DOWN,
+		"movement": Vector2i.DOWN,
+		"action": "ui_down",
+		"long_forbidden_direction": Direction.Up
+	},
+	Direction.Left: {
+		"sprite": HEAD_LEFT,
+		"movement": Vector2i.LEFT,
+		"action": "ui_left",
+		"long_forbidden_direction": Direction.Right
+	},
+	Direction.Right: {
+		"sprite": HEAD_RIGHT,
+		"movement": Vector2i.RIGHT,
+		"action": "ui_right",
+		"long_forbidden_direction": Direction.Left
+	}
+}
+
 @onready var tile_map_layer: TileMapLayer = $TileMapLayer
-@onready var label: Label = $ScoreLabel
+@onready var score_label: Label = $ScoreLabel
 @onready var game_over_label: Label = $GameOverLabel
 
-var facing := Facing.None
-var positions: Array[Vector2i] = [Vector2i(X_MAX/2,Y_MAX/2)]
+var direction := Direction.None
+var positions: Array[Vector2i] = [Vector2i(X_MAX,Y_MAX)/2]
 var ghost_positions: Array[Vector2i] = []
 
-func update_facing():
-	var head_position = positions[0]
-	match facing:
-		Facing.Up, Facing.None:
-			tile_map_layer.set_cell(head_position, 0, HEAD_UP)
-		Facing.Left:
-			tile_map_layer.set_cell(head_position, 0, HEAD_LEFT)
-		Facing.Right:
-			tile_map_layer.set_cell(head_position, 0, HEAD_RIGHT)
-		Facing.Down:
-			tile_map_layer.set_cell(head_position, 0, HEAD_DOWN)
+func set_cell(coord: Vector2i, atlas: Vector2i):
+	tile_map_layer.set_cell(coord, 0, atlas)
 
-func _ready() -> void:
-	update_facing()
+func update_head_sprite():
+	set_cell(positions[0], dirDictionary[direction].sprite)
+func update_tail_sprite():
+	set_cell(positions.pop_back(), NOTHING)
+
+func _ready() -> void:	
+	update_head_sprite()
 	spawn_food()
 	for n in GHOST_COUNT:
 		spawn_ghost()
 
 func _process(delta: float) -> void:
 	var is_long = positions.size() > 1
-	if Input.is_action_just_pressed("ui_up") and (facing != Facing.Down if is_long else true):
-		facing = Facing.Up
-		update_facing()
-	if Input.is_action_just_pressed("ui_down") and (facing != Facing.Up if is_long else true):
-		facing = Facing.Down
-		update_facing()
-	if Input.is_action_just_pressed("ui_left") and (facing != Facing.Right if is_long else true):
-		facing = Facing.Left
-		update_facing()
-	if Input.is_action_just_pressed("ui_right") and (facing != Facing.Left if is_long else true):
-		facing = Facing.Right
-		update_facing()
+	for key in dirDictionary:
+		var value = dirDictionary[key]
+		if  (value.has("action") and Input.is_action_just_pressed(value.action)) \
+		and (direction != value.long_forbidden_direction if is_long else true):
+			direction = key
+			update_head_sprite()
+			break
 
 func is_collision(coord: Vector2i) -> bool:
 	var data = tile_map_layer.get_cell_tile_data(coord)
@@ -77,20 +98,11 @@ func is_food(coord: Vector2i) -> bool:
 	return ate_food
 
 func on_tick() -> void:	
-	if facing == Facing.None: return
+	if direction == Direction.None: return
 	
 	var head_position := positions[0]
-	var next_head_position := head_position
-	match facing:
-		Facing.Up:
-			next_head_position += Vector2i(0,-1)
-		Facing.Left:
-			next_head_position += Vector2i(-1,0)
-		Facing.Right:
-			next_head_position += Vector2i(1,0)
-		Facing.Down:
-			next_head_position += Vector2i(0,1)
-			
+	var next_head_position: Vector2i = head_position + dirDictionary[direction].movement
+	
 	var is_collision = is_collision(next_head_position)
 	var ate_food = is_food(next_head_position)
 	
@@ -100,15 +112,14 @@ func on_tick() -> void:
 		return
 	
 	positions.push_front(next_head_position)
-	update_facing()
-	tile_map_layer.set_cell(head_position, 0, BODY)
+	update_head_sprite()
+	set_cell(head_position, BODY)
 	
 	if !ate_food:
-		var tail_position: Vector2i = positions.pop_back()
-		tile_map_layer.set_cell(tail_position, 0, NOTHING)
+		update_tail_sprite()
 	else:
 		spawn_food()
-		label.text = "SCORE: " + str(positions.size() - 1)
+		score_label.text = "SCORE: " + str(positions.size() - 1)
 
 func get_empty_coord():
 	while true:
@@ -117,36 +128,38 @@ func get_empty_coord():
 		var coord = Vector2i(x,y)
 		if tile_map_layer.get_cell_atlas_coords(coord) == NOTHING:
 			return coord
+
 func get_ghost_movement(original_pos: Vector2i):
-	var xs = [-1,0,1]
-	xs.shuffle()
-	for x in xs:
-		var ys = [-1,0,1]
-		ys.shuffle()
-		for y in ys:
-			var diff = Vector2i(x,y)
-			var coord = original_pos + diff
-			if tile_map_layer.get_cell_atlas_coords(coord) == NOTHING:
-				return coord
+	var diffs = [
+		Vector2i.UP,
+		Vector2i.DOWN,
+		Vector2i.LEFT,
+		Vector2i.RIGHT
+	]
+	diffs.shuffle()
+	for diff in diffs: 
+		var coord = original_pos + diff
+		if tile_map_layer.get_cell_atlas_coords(coord) == NOTHING:
+			return coord
 	return original_pos
 
 func spawn_food():
 	var coord = get_empty_coord()
-	tile_map_layer.set_cell(coord, 0, FOOD)
+	set_cell(coord, FOOD)
 func spawn_ghost():
 	var coord = get_empty_coord()
 	ghost_positions.push_back(coord)
-	tile_map_layer.set_cell(coord, 0, GHOST)
+	set_cell(coord, GHOST)
 
 func update_ghosts():
-	if facing == Facing.None: return
+	if direction == Direction.None: return
 	
 	for i in ghost_positions.size():
 		var original_pos: Vector2i = ghost_positions.pop_front()
 		var new_pos = get_ghost_movement(original_pos)
 		ghost_positions.push_back(new_pos)
-		tile_map_layer.set_cell(original_pos, 0, NOTHING)
-		tile_map_layer.set_cell(new_pos, 0, GHOST)
+		set_cell(original_pos, NOTHING)
+		set_cell(new_pos, GHOST)
 
 func on_restart_pressed() -> void:
 	get_tree().paused = false
